f9512ef8c408d42d6e92470e0b8116140b47e3ab
jackson-evarts <jack.evarts@gmail.com>
2025-01-01 18:53:18 -0500

    Initial Commit

---
+++ DapperTracker/DapperTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DapperTrackerApp.swift
+//  DapperTracker
+//
+//  Created by Jackson Evarts on 1/1/25.
+//
+
+import SwiftUI
+
+@main
+struct DapperTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

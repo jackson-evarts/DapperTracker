07884c1b88a9495ba6049a42731e9503c82e6353
jackson-evarts <jack.evarts@gmail.com>
2025-01-01 23:20:09 -0500

    Initial development of the UI

--- DapperTracker/ContentView.swift
+++ DapperTracker/ContentView.swift
@@ -8,14 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    // TODO: Make a variable named CurrentDapScore that will save an integer in the app for the duration of the day
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            // Background color filling the entire screen
+            Color.lightGrey // Background color
+                .edgesIgnoringSafeArea(.all) // Fill entire background
+            
+            VStack {
+                Text("Dap Tracker")
+                    .font(.custom("Futura-Bold", size: 40))
+                    .foregroundColor(.black)
+                    .multilineTextAlignment(.center) // Center-align text
+                    .frame(maxWidth: .infinity, alignment: .center) // Center horizontally
+                
+                HStack{
+                    
+                    // TODO: Make these buttons
+
+                    VStack {
+                        Image("BadDap")
+                            .resizable() // Allows resizing of the image
+                            .aspectRatio(contentMode: .fit) // Maintains the aspect ratio while fitting within the bounds
+                            .frame(maxWidth: 200, maxHeight: 200) // Sets a reasonable size limit for the image
+                            .frame(maxWidth: .infinity) // Ensures the image is centered horizontally
+                        Text("Miss")
+                            .font(.custom("Futura-Bold", size: 20))
+                            .foregroundColor(.black)
+                            .multilineTextAlignment(.center) // Center-align text
+                            .frame(maxWidth: .infinity, alignment: .center) // Center horizontally
+                    }
+                    
+                    
+                    VStack {
+                        // TODO: Display in image named pop when the button is pressed 
+                        
+                        Image("GoodDap")
+                            .resizable() // Allows resizing of the image
+                            .aspectRatio(contentMode: .fit) // Maintains the aspect ratio while fitting within the bounds
+                            .frame(maxWidth: 200, maxHeight: 200) // Sets a reasonable size limit for the image
+                            .frame(maxWidth: .infinity) // Ensures the image is centered horizontally
+                        
+                        Text("Crisp Dap")
+                            .font(.custom("Futura-Bold", size: 20))
+                            .foregroundColor(.black)
+                            .multilineTextAlignment(.center) // Center-align text
+                            .frame(maxWidth: .infinity, alignment: .center) // Center horizontally
+                    }
+                }
+                
+                
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
---
+++ DapperTracker/ContentViewLogic.swift
@@ -0,0 +1,34 @@
+//
+//  ContentViewLogic.swift
+//  DapperTracker
+//
+//  Created by Jackson Evarts on 1/1/25.
+//
+import SwiftUI
+
+extension Color {
+    static let primaryGold = Color(hex: "#DAA520") // Goldenrod
+    static let softGold = Color(hex: "#FFD700")    // Gold
+    static let mutedGold = Color(hex: "#B8860B")   // Dark Goldenrod
+
+    static let lightGrey = Color(hex: "#F5F5F5")   // White Smoke
+    static let neutralGrey = Color(hex: "#D3D3D3") // Light Grey
+    static let darkGrey = Color(hex: "#696969")    // Dim Grey
+    static let charcoalGrey = Color(hex: "#333333") // Dark Grey
+
+    // Helper initializer for HEX values
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        _ = scanner.scanString("#")
+
+        var rgb: UInt64 = 0
+        scanner.scanHexInt64(&rgb)
+
+        let red = Double((rgb >> 16) & 0xFF) / 255.0
+        let green = Double((rgb >> 8) & 0xFF) / 255.0
+        let blue = Double(rgb & 0xFF) / 255.0
+
+        self.init(red: red, green: green, blue: blue)
+    }
+}
+
